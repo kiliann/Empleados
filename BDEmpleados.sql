669ecae83580895d30837523d23da2dafa485a56
kiliann <wilian_kilian@hotmail.com>
2021-10-15 14:06:05 +0200

    version 1

---
+++ BDEmpleados.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE BDEmpleado CHARACTER SET 'UTF8' COLLATE 'utf8_general_ci';
+
+CREATE TABLE Empleado(
+id_empleado TINYINT unsigned not NULL,
+DNI CHAR(9) not null,
+Nombre VARCHAR(18) not null,
+Correo VARCHAR(30) NULL,
+Telefono CHAR(15),
+PRIMARY KEY(id_empleado
+)
